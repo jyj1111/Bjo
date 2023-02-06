f576e79c057ec0a944f9e0f691d7fa89b4637b8e
jyj1111 <89414343+jyj1111@users.noreply.github.com>
2023-02-06 23:35:47 +0900

    [Gold V] Title: 용액, Time: 232 ms, Memory: 26820 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.js
@@ -0,0 +1,23 @@
+const fs=require("fs");
+let input=fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+let arr=input[1].split(' ').map((item)=>Number(item));
+let answer=[0,0];
+let distance=2000000000;
+let lt=0;
+let rt=arr.length-1;
+while(lt<rt){
+     
+     if(Math.abs(arr[lt]+arr[rt])<distance){
+         distance=Math.abs(arr[lt]+arr[rt]);
+         answer=[arr[lt],arr[rt]];
+     }
+     if(arr[lt]+arr[rt]<0){
+         lt++;
+     }else if(arr[lt]+arr[rt]>0){
+         rt--;
+     }else{
+         break;
+     }       
+        
+}
+console.log(answer.join(' '));
