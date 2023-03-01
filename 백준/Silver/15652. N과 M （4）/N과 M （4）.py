c2cb20070cb722129ae2702a672e41ada31e48bd
jyj1111 <89414343+jyj1111@users.noreply.github.com>
2023-03-01 16:33:22 +0900

    [Silver III] Title: N과 M (4), Time: 80 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,22 @@
+import sys
+input=sys.stdin.readline
+n,m=map(int,input().split())
+checked=[0]*(n+1)
+answer=[0]*(m+1)
+def DFS(idx):
+    
+    if idx==m+1:
+        for i in range(1,m+1):
+            print(answer[i],end=' ')
+        print()
+        
+    elif idx>m+1:
+        return
+    else:
+        for i in range(1,n+1):
+            if max(answer)<=i:
+                answer[idx]=i
+                DFS(idx+1)
+                answer[idx]=0
+
+DFS(1)
