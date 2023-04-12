c0d1d15319fad934934c0a6fd12acbef193820ec
jeong yoon jo <89414343+jyj1111@users.noreply.github.com>
2023-04-12 16:13:56 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 663.56 ms, Memory: 132 MB -BaekjoonHub

--- 프로그래머스/lv3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.py
+++ 프로그래머스/lv3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.py
@@ -1,26 +1,15 @@
 def solution(board, skill):
-    imos=[[0]*(len(board[0])+1) for _ in range(len(board)+1)]
     for s in skill:
         type,x1,y1,x2,y2,degree=s;
         if type==1:
             degree=-degree
-        imos[x1][y1]+=degree
-        imos[x1][y2+1]-=degree
-        imos[x2+1][y1]-=degree
-        imos[x2+1][y2+1]+=degree
-        
-    for i in range(len(imos)):
-        for j in range(1,len(imos[0])):
-            imos[i][j]+=imos[i][j-1]
-                
-    for i in range(len(imos[0])):
-        for j in range(1,len(imos)):
-            imos[j][i]+=imos[j-1][i]    
-         
+        for i in range(x1,x2+1):
+            for j in range(y1,y2+1):
+                board[i][j]+=degree
+           
     answer = 0      
     for i in range(0,len(board)):
         for j in range(0, len(board[0])):
-            board[i][j]+=imos[i][j]
             if board[i][j]>0:
                 answer+=1
     return answer
