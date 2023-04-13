b4a615f4a4db7d79ca41aac3eed292dad0355ae0
jeong yoon jo <89414343+jyj1111@users.noreply.github.com>
2023-04-13 23:12:22 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -2,20 +2,20 @@ def solution(id_list, report, k):
     answer = []
     systemDict={}
     attackedDict={}
-    attackedArr=[]
+    attackedSet=set()
     for id in id_list:
-        systemDict[id]=[]
+        systemDict[id]=set()
         attackedDict[id]=0
     for alarm in set(report):
         user,attacked=alarm.split(' ')
-        systemDict[user].append(attacked)
+        systemDict[user].add(attacked)
         attackedDict[attacked]+=1
     for key in attackedDict:
         if attackedDict[key]>=k:
-            attackedArr.append(key)
+            attackedSet.add(key)
     
     for key in systemDict:
-        answer.append(len(set(systemDict[key])&set(attackedArr)))
+        answer.append(len(systemDict[key]&attackedSet))
     return answer
             
            
