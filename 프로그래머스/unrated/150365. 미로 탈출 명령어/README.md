e5725c97670a71025afd06c9b4d823cc9b791db6
jyj1111 <89414343+jyj1111@users.noreply.github.com>
2023-09-27 12:39:42 +0900

    [unrated] Title: 미로 탈출 명령어, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/150365. 미로 탈출 명령어/README.md
+++ 프로그래머스/unrated/150365. 미로 탈출 명령어/README.md
@@ -1,10 +1,10 @@
 # [unrated] 미로 탈출 명령어 - 150365 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150365) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150365#) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.00 ms
+메모리: 10.2 MB, 시간: 0.00 ms
 
 ### 구분
 
@@ -12,7 +12,7 @@
 
 ### 채점결과
 
-<br/>정확성: 100.0<br/>합계: 100.0 / 100.0
+정확성: 100.0<br/>합계: 100.0 / 100.0
 
 ### 문제 설명
 
--- 프로그래머스/unrated/150365. 미로 탈출 명령어/미로 탈출 명령어.py
+++ 프로그래머스/unrated/150365. 미로 탈출 명령어/미로 탈출 명령어.py
@@ -1,34 +1,37 @@
-def dist(x, y, r, c):
-    return abs(x - r) + abs(y - c)
-
+"""
+사전순: d(+,0),l(0,-),r(0,+),u(-,0)
+"""
 def solution(n, m, x, y, r, c, k):
-    if (k - dist(x, y, r, c)) % 2 or k < dist(x, y, r, c):
+    answer = ''
+    moveX=x
+    moveY=y
+    dis=k
+    if (dis- abs(r - moveX)-abs(c - moveY))%2==1 or abs(r - moveX)+abs(c - moveY)>dis:
         return "impossible"
-    answer = ""
-    move = 0
-    while x < n and (k - move) > dist(x, y, r, c):
-        move += 1
-        x += 1
-        answer += "d"
-    while 1 < y and (k - move) > dist(x, y, r, c):
-        move += 1
-        y -= 1
-        answer += "l"
-
-    while (k - move) > dist(x, y, r, c):
-        move += 2
-        answer += "rl"
-
-    if x < r:
-        answer += "d" * (r - x)
-        x = r
-    if y > c:
-        answer += "l" * (y - c)
-        y = c
-    if y < c:
-        answer += "r" * (c - y)
-        y = c
-    if x > r:
-        answer += "u" * (x - r)
-        x = r
+    while dis- (abs(r - moveX)+abs(c - moveY))>0:
+        if 1<=moveX+1<=n:
+            moveX+=1
+            answer+='d'
+        elif 1<=moveY-1<=m:
+            moveY-=1
+            answer+='l'
+        elif 1<=moveY+1<=m:
+            moveY+=1
+            answer+='r'
+        elif 1<=moveX-1<=n:
+            moveX-=1
+            answer+='u'
+        dis-=1
+    if r-moveX >= 0 and c-moveY >= 0:
+        answer+='d'*abs(r-moveX)
+        answer+='r'*abs(c-moveY)
+    elif r-moveX >= 0 and c-moveY < 0:
+        answer+='d'*abs(r-moveX)
+        answer+='l'*abs(c-moveY)
+    elif r-moveX < 0 and c-moveY >= 0:
+        answer+='r'*abs(c-moveY)
+        answer+='u'*abs(r-moveX)
+    elif r-moveX < 0 and c-moveY < 0:
+        answer+='l'*abs(c-moveY)
+        answer+='u'*abs(r-moveX)
     return answer
