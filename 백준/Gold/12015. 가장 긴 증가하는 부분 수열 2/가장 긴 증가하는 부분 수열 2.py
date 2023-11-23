c8887b7b643cdac12ac618eae733841df1742385
jyj1111 <89414343+jyj1111@users.noreply.github.com>
2023-11-23 19:01:58 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 1096 ms, Memory: 157604 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
@@ -0,0 +1,19 @@
+import sys
+from bisect import bisect_left
+input=sys.stdin.readline
+
+N=int(input())# 1<=N<=1000000
+arr=list(map(int,input().split()))
+
+stack=[]
+
+for i in range(N):
+    if stack and stack[-1]>=arr[i]:
+        idx=bisect_left(stack,arr[i])
+        stack[idx]=arr[i]
+
+    else:
+        stack.append(arr[i])
+
+print(len(stack))
+        
