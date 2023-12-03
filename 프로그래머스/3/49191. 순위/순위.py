2a5e7d62a6be5ad66a6d08c4b111e3d623dfaf58
jyj1111 <89414343+jyj1111@users.noreply.github.com>
2023-12-03 20:31:50 +0900

    [level 3] Title: 순위, Time: 5.42 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.py
+++ 프로그래머스/3/49191. 순위/순위.py
@@ -2,19 +2,29 @@ from collections import deque,defaultdict
 
 def solution(n, results):
     answer = 0
-    winDic = defaultdict(set)    
-    loseDic = defaultdict(set)   
-    for winner,loser in results:        
-        winDic[loser].add(winner)
-        loseDic[winner].add(loser)
-    for i in range(1,n+1):         
-        for winner in winDic[i]:                    
-            loseDic[winner].update(loseDic[i])
-        for loser in loseDic[i]:                    
-            winDic[loser].update(winDic[i])
+    graph=[[[],[]] for _ in range(n+1)]
     
+    queue=deque()
+    indegree=[0]*(n+1)
+    for win,lose in results:
+        graph[win][0].append(lose)
+        graph[lose][1].append(win)
+        indegree[lose]+=1
+    print(graph)
+    """
     for i in range(1,n+1):
-        if len(winDic[i])+len(loseDic[i]) == n-1:   
-            answer+=1
-        
+        if indegree[i]==0:
+            queue.append(i)
+
+    ranks=[]
+    while queue:
+        n=queue.popleft()
+        ranks.append(n)
+        for j in graph[n]:
+            indegree[j]-=1
+            if indegree[j]==0:
+                queue.append(j)
+    print(graph)
+    print(ranks)    
+    """    
     return answer
