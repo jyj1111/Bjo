3fbc941c2f1a88bac46f03bfe153d37fa8bd2898
jyj1111 <89414343+jyj1111@users.noreply.github.com>
2023-12-13 21:59:57 +0900

    [Gold I] Title: 택배, Time: 2704 ms, Memory: 33164 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/8980. 택배/택배.py
+++ 백준/Gold/8980. 택배/택배.py
@@ -8,20 +8,18 @@ for i in range(M):
     start,end,value=map(int,input().split())
     arr.append((start,end,value))
 
-arr.sort(key=lambda x:(-x[1],-x[0]))
+arr.sort(key=lambda x:(x[1],x[0]))
 capacity=[0]*(N+1)
 
 ans=0
 for start,end,value in arr:
     #print(start,end,value)
     #print(capacity)
-    tmp=0
-    if capacity[end]+value<=C:
-        tmp=value
-    else:
-        tmp=C-capacity[end]
-       
-    for j in range(end,start,-1):
+    tmp=value
+    for j in range(start,end):
+        tmp=min(tmp,C-capacity[j])
+    #print(tmp)       
+    for j in range(start,end):
         capacity[j]+=tmp
     ans+=tmp
    
