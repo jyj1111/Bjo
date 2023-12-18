f71a9ebcf88df402583353b4e58da4b6e6112a67
jyj1111 <89414343+jyj1111@users.noreply.github.com>
2023-12-18 20:53:34 +0900

    [Gold III] Title: 양팔저울, Time: 52 ms, Memory: 34396 KB -BaekjoonHub

--- 백준/Gold/2629. 양팔저울/양팔저울.py
+++ 백준/Gold/2629. 양팔저울/양팔저울.py
@@ -1,36 +1,38 @@
 import sys
-sys.setrecursionlimit(10 ** 9)
 input=sys.stdin.readline
+sys.setrecursionlimit(10 ** 9)
+n=int(input())
+arr=list(map(int,input().split()))
+dp=[[0]*((500*30)+1) for _ in range(31)]
 
-N=int(input())#1≤N≤30
-weights=list(map(int,input().split()))
-M=int(input())#1≤M<=7
-checks=list(map(int,input().split()))
-dp=[[0]*(15001) for i in range(31)]
-
-def DFS(idx,hap):
+def DFS(depth,result):
     global dp
-    #print(idx,hap)
-    if idx>N:
+    #print(depth,result)
+    if depth>n:
         return
-    if dp[idx][hap]:
-        return
+    if dp[depth][result]:
+        return 
+    dp[depth][result]=1
+    DFS(depth+1,result+arr[depth-1])
+    DFS(depth+1,result)
+    DFS(depth+1,abs(result-arr[depth-1]))
     
-    dp[idx][hap]=1
-    DFS(idx+1,hap+weights[idx-1])
-    DFS(idx+1,hap)
-    DFS(idx+1,abs(hap-weights[idx-1]))
-           
     
-
 DFS(0,0)
+#print(dp)
+
+m=int(input())
 ans=[]
-for check in checks:
-    if check>15000:
-        ans.append('N')
-    elif dp[N][check]:
+cases=list(map(int,input().split()))
+for case in cases:
+    if case>15000:
+        ans.append('N')     
+        
+    elif dp[n][case]:
         ans.append('Y')
+        
     else:
         ans.append('N')
 
 print(*ans)
+        
