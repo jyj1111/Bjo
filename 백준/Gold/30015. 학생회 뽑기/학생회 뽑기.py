0d04d5f3702d51a67c448b40675a46f16440e0b8
jyj1111 <89414343+jyj1111@users.noreply.github.com>
2024-01-17 22:29:01 +0900

    [Gold III] Title: 학생회 뽑기, Time: 360 ms, Memory: 53244 KB -BaekjoonHub

--- 백준/Gold/30015. 학생회 뽑기/학생회 뽑기.py
+++ 백준/Gold/30015. 학생회 뽑기/학생회 뽑기.py
@@ -1,23 +1,20 @@
 import sys
 
 input=sys.stdin.readline
-n,k=map(int,input().split()) # 1<=n<=200000, 1<=k<=n
-arr=list(map(int,input().split()))
+n, k = map(int, input().split())  # 1<=n<=200000, 1<=k<=n
+arr = list(map(int, input().split()))
 
-ans=0
+# 최종 결과를 저장할 변수를 초기화합니다.
+ans = 0
 
-for i in range(19,-1,-1):
-    cnt=0
-    tmp=[]
-    for num in arr:
-        if num&(1<<i):
-            tmp.append(num)
+# 각 비트에 대해
+for i in range(19, -1, -1):
+    # 현재 비트가 1인 숫자들만 선택합니다.
+    tmp = [num for num in arr if num & (1 << i)]
 
-    if len(tmp)>=k:
-         ans=ans|(1<<i)
-         arr=tmp
-
-                    
-            
+    # 선택된 숫자가 k개 이상이면, 해당 비트를 결과에 추가합니다.
+    if len(tmp) >= k:
+        ans |= (1 << i)
+        arr = tmp
 
 print(ans)
