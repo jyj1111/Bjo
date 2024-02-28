a2af225ffed86f10a4ae9757a6a5eff798bd6b75
jyj1111 <89414343+jyj1111@users.noreply.github.com>
2024-02-28 17:23:10 +0900

    [Gold II] Title: 쇼핑몰, Time: 400 ms, Memory: 49332 KB -BaekjoonHub

--- 백준/Gold/17612. 쇼핑몰/쇼핑몰.py
+++ 백준/Gold/17612. 쇼핑몰/쇼핑몰.py
@@ -1,27 +1,32 @@
 import sys
 input=sys.stdin.readline
-from heapq import heapify,heappush,heappop
+from heapq import heappush,heappop
 
 n,k=map(int,input().split())
-counters=[]
-customers=[]
+counterIn=[]
+counterOut=[]
 
-for i in range(1,n+1):
-    Id,w=map(int,input().split())
-    if len(counters)<k:
-        heappush(counters,(w,i,Id))
-    else:
-        w1,counterId,customerId=heappop(counters)
-        heappush(counters,(w+w1,counterId,Id))
-        heappush(customers,(w1,-counterId,customerId))
-        
 
-while counters:
-    w1,counterId,customerId=heappop(counters)
-    heappush(customers,(w1,-counterId,customerId))
-
-ans=0
 for i in range(n):
-    ans+=(i+1)*heappop(customers)[2]
+    pid,w=map(int,input().split())
+    if len(counterIn)<k:
+        heappush(counterIn,(w,i,pid))
+        
+    else:
+        w1,cid,pid1=heappop(counterIn)
+        heappush(counterIn,(w+w1,cid,pid))
+        heappush(counterOut,(w1,-cid,pid1))
 
-print(ans)
+while counterIn:
+    w1,cid,pid1=heappop(counterIn)
+    heappush(counterOut,(w1,-cid,pid1))
+
+answer=0
+idx=0
+while counterOut:
+    idx+=1
+    w,cid,pid=heappop(counterOut)
+    answer+=idx*pid
+    
+
+print(answer)
