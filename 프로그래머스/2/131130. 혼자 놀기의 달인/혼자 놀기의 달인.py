5e8e7dd02e062576ee3f47d46bed8f271aad736f
jyj1111 <89414343+jyj1111@users.noreply.github.com>
2024-07-07 04:10:47 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.py
+++ 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.py
@@ -1,12 +1,3 @@
 def solution(cards):
-    answer = []
-    visited=[False]*(len(cards))
-    for i in range(len(cards)):
-        cnt=0
-        while not visited[i]:
-            visited[i]=True
-            i=cards[i]-1
-            cnt+=1
-        answer.append(cnt)
-    answer.sort()        
-    return answer[-1]*answer[-2]
+    answer = 0
+    return answer
