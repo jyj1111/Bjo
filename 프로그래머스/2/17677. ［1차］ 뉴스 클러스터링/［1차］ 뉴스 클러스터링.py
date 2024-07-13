3ce028f3739772166673cf56fc28715daf952839
jyj1111 <89414343+jyj1111@users.noreply.github.com>
2024-07-13 17:12:14 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.83 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -1,25 +1,19 @@
 def solution(str1, str2):
-    s1,s2="",""
-    for s in str1:
-        s1+=s.upper()
-    for s in str2:
-        s2+=s.upper()
-    S1,S2=[],[]
-    for i in range(len(s1)-1):
-        if s1[i:i+2].isalpha():
-            S1.append(s1[i:i+2])          
-    for i in range(len(s2)-1):
-        if s2[i:i+2].isalpha():
-            S2.append(s2[i:i+2])    
-    n1,n2,n3=len(S1),len(S2),0
+    s1 = ''.join(c.upper() for c in str1)
+    s2 = ''.join(c.upper() for c in str2)
+
+    S1 = [s1[i:i+2] for i in range(len(s1)-1) if s1[i:i+2].isalpha()]
+    S2 = [s2[i:i+2] for i in range(len(s2)-1) if s2[i:i+2].isalpha()]
+
+    n1 ,n2 ,n3 = len(S1), len(S2), 0
     for s in S1:
         if s in S2:
             S2.remove(s)
-            n3+=1
-    
-    if n1==0 and n2==0:
+            n3 += 1
+
+    if n1 == 0 and n2 == 0:
         return 65536
     else:
-        return n3*65536//(n1+n2-n3)
+        return n3 * 65536 // (n1 + n2 - n3)
     
  
